22a3f2e6ab40fe43330b92086566a77736f3d50b
Steffen Roskamp <steffen.roskamp@gmail.com>
2016-08-27 19:21:53 +0200

    Added dependency statement for async.el

--- transfer-sh.el
+++ transfer-sh.el
@@ -4,6 +4,7 @@
 
 ;; Author: S. Roskamp <steffen.roskamp@gmail.com>
 ;; Keywords: cloud, upload, share
+;; Package-Requires: ((async))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
